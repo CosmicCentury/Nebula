07efe44f32857969e6fe5cc1ef308aa9e215dc37
raphael <raphaelvguy@gmail.com>
2023-12-10 15:39:36 +0800

    ujpdate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Home = () => {
   return (
     <Box>
-      <div>testing 121231312312313 </div>
+      <div>testing 1212313123ssdadasd12313 </div>
     </Box>
   );
 };
